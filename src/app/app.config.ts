43161c8e742a83452a24eb62c9d0461904d4ee64
Gisela Mariano <gisela-mariano@hotmail.com>
2025-08-05 16:10:31 -0300

    chore(deps): update Angular dependences and add Apollo Client

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,17 @@
 import {
 	ApplicationConfig,
+	inject,
 	provideBrowserGlobalErrorListeners,
 	provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { InMemoryCache } from '@apollo/client/core';
 import Aura from '@primeuix/themes/aura';
+import { provideApollo } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
 import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
 
@@ -20,6 +25,15 @@ export const appConfig: ApplicationConfig = {
 			theme: {
 				preset: Aura,
 			},
+		}),
+		provideHttpClient(),
+		provideApollo(() => {
+			const httpLink = inject(HttpLink);
+
+			return {
+				link: httpLink.create({}),
+				cache: new InMemoryCache(),
+			};
 		}),
 	],
 };
